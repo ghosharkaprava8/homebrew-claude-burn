0e83a4dc47e3b772c1b1f0037da3df57102f6ae5
Arkaprava Ghosh <ghosharkaprava8@gmail.com>
2026-04-29 05:53:25 +0000

    chore(claude-burn): bottles for 0.1.1

--- Formula/claude-burn.rb
+++ Formula/claude-burn.rb
@@ -9,6 +9,8 @@ class ClaudeBurn < Formula
 
   bottle do
     root_url "https://github.com/ghosharkaprava8/homebrew-claude-burn/releases/download/claude-burn-0.1.1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2c01c02f9d88c851644d9782fb2089432431e6f4cd1fe0a33dcc4c7a1b92a36e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ae4cec26a020aaf87445ab020eb688bb06d258b63314b3d044f462537cd7996d"
   end
 
   depends_on "python@3.13"
